dfbe8e4a1a2e198a12a9989522e8476180f3a6b0
Simao Moreno <saiman13moreno@gmail.com>
2021-10-14 22:17:12 +0100

    project structure created by dotnet v3

--- myfirstapp/Program.cs
+++ myfirstapp/Program.cs
@@ -6,7 +6,8 @@ namespace myfirstapp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Console.WriteLine("Hello World!");
+            Console.WriteLine("Ola mundo!");
         }
     }
 }
